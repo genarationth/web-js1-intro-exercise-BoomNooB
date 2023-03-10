bad909fe39749c30e877d8aa729557e3fa1ee191
BoomThinkpad <boom@live.se>
2023-03-10 12:04:35 +0700

    Remove unnecessary line

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -1,6 +1,5 @@
 const ChangeName = () => {
-    userName = prompt("What's your name sir?")
-    const element = document.getElementById("inputName");
+    let userName = prompt("What's your name sir?")
     document.getElementById("inputName").innerHTML = `Hello ${userName} !`;
 
 }
